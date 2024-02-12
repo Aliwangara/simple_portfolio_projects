da8f952ab705da69384aba8f6490a0fdf3c864fd
Aliwangara <aliwangara63@gmail.com>
2024-02-12 18:25:33 +0300

    I added google account instructions to settings  and to views

--- main/views.py
+++ main/views.py
@@ -3,7 +3,9 @@ from email import message
 from django.shortcuts import render
 from .models import Contacts
 from django.contrib import messages
-
+from django.core.mail import send_mail
+from  django.conf import settings
+from simple_portfolio_project.settings import EMAIL_HOST_USER
 # Create your views here.
 class Home:
     pass
@@ -17,12 +19,18 @@ def portfolio(request):
     return render(request, 'Portfolio.html')
 
 
-def contact(request):
+def contact(request,):
     if request.method=="POST":
         fname = request.POST.get('name')
         femail = request.POST.get('email')
         fsubject = request.POST.get('subject')
         fmessage = request.POST.get('message')
+        send_mail(
+                    'Contacts Form'
+                  , '\n'
+                  , 'settings.EMAIL_HOST_USER'
+                  , ['aliwangara63@gmail.com']
+                  , fail_silently=False )
         query = Contacts(name=fname, email=femail, subject=fsubject, message=fmessage)
         query.save()
         messages.success(request, "Thanks for contacting. I will get back to you soon!")
--- simple_portfolio_project/settings.py
+++ simple_portfolio_project/settings.py
@@ -25,6 +25,13 @@ SECRET_KEY = 'django-insecure--ia8$vl))-*3al4ghr0l(+53binax6p3kg!d$fv*le#vt!esu!
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'aliwangara63@gmail.com'
+EMAIL_HOST_PASSWORD = 'jdzydbwxpctileht'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
 ALLOWED_HOSTS = []
 
