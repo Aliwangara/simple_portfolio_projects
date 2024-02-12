99898d13ed85b833c94a00dafa8e7278794c99ab
Aliwangara <aliwangara63@gmail.com>
2024-02-12 23:56:05 +0300

    I did some minor changes on settings.py and views.py for gmail account so that they could send email dynamically.

--- main/views.py
+++ main/views.py
@@ -4,8 +4,9 @@ from django.shortcuts import render
 from .models import Contacts
 from django.contrib import messages
 from django.core.mail import send_mail
+from django.core import  mail
+from django.core.mail.message import EmailMessage
 from  django.conf import settings
-from simple_portfolio_project.settings import EMAIL_HOST_USER
 # Create your views here.
 class Home:
     pass
@@ -25,13 +26,22 @@ def contact(request,):
         femail = request.POST.get('email')
         fsubject = request.POST.get('subject')
         fmessage = request.POST.get('message')
-        send_mail(
-                    'Contacts Form'
-                  , '\n'
-                  , 'settings.EMAIL_HOST_USER'
-                  , ['aliwangara63@gmail.com']
-                  , fail_silently=False )
+
         query = Contacts(name=fname, email=femail, subject=fsubject, message=fmessage)
         query.save()
+        #Email sending start
+        from_email = settings.EMAIL_HOST_USER
+        connection = mail.get_connection()
+        connection.open()
+        email_message = mail.EmailMessage(f'email is from {fname}', f'user email : {femail}\nuserPhoneNumber : {fsubject}\n\n\n query : {fmessage}'
+                                          ,from_email, ['aliwangara63@gmail.com', 'wangaraali56@gmail.com'], connection=connection)
+
+        email_client= mail.EmailMessage('Aliwangara response','Thanks for reaching me\n\n+254700331844\naliwangara63@gmail.com'
+                                          ,from_email, [femail], connection=connection)
+
+
+        connection.send_messages([email_message])
+        connection.close()
+
         messages.success(request, "Thanks for contacting. I will get back to you soon!")
     return render(request, 'Contact.html')
--- simple_portfolio_project/settings.py
+++ simple_portfolio_project/settings.py
@@ -30,7 +30,6 @@ EMAIL_HOST_USER = 'aliwangara63@gmail.com'
 EMAIL_HOST_PASSWORD = 'jdzydbwxpctileht'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_USE_SSL = False
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
 ALLOWED_HOSTS = []
